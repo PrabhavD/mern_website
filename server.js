8befd8a18eb1ae5473ce4336e1812d0f6497e2e7
Prabhav Desai <desaipd1220@gmail.com>
2020-12-10 16:36:21 -0700

    unexpected ) token on mongoose.connect

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
 mongoose
-    .connect(db, {useUnifiedTopology: true, useNewUrlParser: true }))
+    .connect(db, {useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
