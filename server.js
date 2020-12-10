f2145c5aff69098bec32fa7b1c4eda169156d5a8
Prabhav Desai <desaipd1220@gmail.com>
2020-12-10 16:33:02 -0700

    mongoDB URI connection

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const app = express();
 
 //BodyParser included in new express.js
 app.use(express.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose
+    .connect(db, {useUnifiedTopology: true, useNewUrlParser: true }))
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => console.log('Server started on port ${port}'));
