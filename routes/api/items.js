c37beac0240413ab2c1ea63fea65becdd592e868
Prabhav Desai <desaipd1220@gmail.com>
2020-12-10 17:15:40 -0700

    GET, POST, DELETE

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,4 +13,24 @@ router.get('/', (req, res) => {
         .then(items => res.json(items))
 });
 
+// route:           POST request to api/items
+// description:     Create an item
+// access:          Public  -> make private later when adding auth
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name //date is automatically inserted
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+// route:           DELETE request to api/items:id
+// description:     Delete an item
+// access:          Public  -> make private later when adding auth
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .them(item => item.remove().then(() => res.json({success: true})))
+        .catch(err => res.status(404).json({sucess: false}));
+})
+
 module.exports = router;
