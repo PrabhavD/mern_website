4691a15f6c8f8b96c765e1f9335a91fd12620004
Prabhav Desai <desaipd1220@gmail.com>
2020-12-11 17:44:42 -0700

    integrate modal into app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ModularList from './components/ModularList';
+import ItemModal from './components/itemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -14,7 +16,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavbar />
+          <Container>
+          <ItemModal />
           <ModularList />
+          </Container>
         </div>
       </Provider>
     )
