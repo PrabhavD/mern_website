bdac16dbc3b92a94e03114a94455e5b2b712992d
Prabhav Desai <desaipd1220@gmail.com>
2020-12-11 18:02:49 -0700

    clear static data for backend integration

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,9 @@
 import { v4 as uuid } from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        {id: uuid(), name: 'Math 115'},
-        {id: uuid(), name: 'ECE 190'},
-        {id: uuid(), name: 'ARTS 190'},
-        {id: uuid(), name: 'MATH 117'}
-    ]
+    items: [],
+    loading: false
 };
 
 export default function(state = initialState, action) {
