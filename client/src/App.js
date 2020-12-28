b56933f975419a71f9b7508cf0a85a7007b2eb2d
Prabhav Desai <desaipd1220@gmail.com>
2020-12-27 18:31:41 -0700

    Version 1.0.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import AppNavbar from './components/AppNavbar';
+import AppNavbar from './components/appnavbar';
 import ModularList from './components/ModularList';
 import ItemModal from './components/itemModal';
 import { Container } from 'reactstrap';
