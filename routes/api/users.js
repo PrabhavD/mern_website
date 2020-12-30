0fb77edb46d61a14213bd9c45d4d41fb8a846e28
Prabhav Desai <desaipd1220@gmail.com>
2020-12-29 20:11:13 -0700

    user validation implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 
 //User Model
 const User = require('../../models/User');
@@ -8,7 +9,26 @@ const User = require('../../models/User');
 // description:     Register new user
 // access:          Public
 router.post('/', (req, res) => {
-    res.send('register');
+    const { name, email, password} = req.body;
+
+    //Simple validation
+    if(!name || !email || !password) {
+        return res.status(400).json( { msg: 'Plese enter all fields'});
+    }
+
+    //Check for existing user
+    User.findOne({ email })
+        .then(user => { 
+            if(user) return res.status(400).json({ msg: 'User already exists'});
+
+            const newUser = new User({
+                name, 
+                email,
+                password,
+            });
+
+            
+        })
 });
 
 module.exports = router;
