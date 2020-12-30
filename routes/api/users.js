69b943d29187c998ba7941000a3d02cae46635d6
Prabhav Desai <desaipd1220@gmail.com>
2020-12-29 20:17:25 -0700

    salt and hash using bcrypt.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -27,7 +27,23 @@ router.post('/', (req, res) => {
                 password,
             });
 
-            
+            // Create salt & hash
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => {
+                            res.json({
+                                user: {
+                                    id: user.id,
+                                    name: user.name,
+                                    email: user.email
+                                }
+                            });
+                        });
+                })
+            })
         })
 });
 
