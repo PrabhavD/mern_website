babfd7ac5e25f7b263eb048f042937eef4f09521
Prabhav Desai <desaipd1220@gmail.com>
2020-12-29 20:50:22 -0700

    creation of middleware

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+const config = require('config')
+const jwt = require('jsonwebtoken')
+
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+
+    //Check for token
+    
+}
