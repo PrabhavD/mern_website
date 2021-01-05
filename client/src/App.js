e15e210aa1c901b19092a7df95604f55b19182fb
Prabhav Desai <desaipd1220@gmail.com>
2021-01-04 20:26:40 -0700

    Update to incorporate useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import AppNavbar from './components/AppNavbar';
-import ModularList from './components/ModularList';
-import ItemModal from './components/itemModal';
-import { Container } from 'reactstrap';
-
+import React, { useEffect } from 'react';
+import './App.css';
+import AppNavBar from './components/AppNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ShoppingList from './components/ShoppingList';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Container } from 'reactstrap';
+import { ItemModal } from './components/ItemModal';
 import { loadUser } from './actions/authActions';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-class App extends Component {
-  componentDidMount() {
+function App() {
+  useEffect(() => {
     store.dispatch(loadUser());
-  }
+  }, []);
 
-  render () {
-    return (
-      <Provider store={store}>
-        <div className="App">
-          <AppNavbar />
-          <Container>
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar />
+        <Container>
           <ItemModal />
-          <ModularList />
-          </Container>
-        </div>
-      </Provider>
-    )
-  };
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
